3fe1cf5817e940c31f6df20f75a926f64d73c6da
Sung Bae <singsang2@gmail.com>
2018-10-17 18:06:49 +0000

    automatically backed up by learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -101,18 +101,18 @@ end
 cart = generate_cart
 coupons = generate_coupons
 
-# puts "Items in cart"
-# cart.each do |item|
-# 	puts "Item: #{item.keys.first}"
-# 	puts "Price: #{item[item.keys.first][:price]}"
-# 	puts "Clearance: #{item[item.keys.first][:clearance]}"
-# 	puts "=" * 10
-# end
+puts "Items in cart"
+cart.each do |item|
+	puts "Item: #{item.keys.first}"
+	puts "Price: #{item[item.keys.first][:price]}"
+	puts "Clearance: #{item[item.keys.first][:clearance]}"
+	puts "=" * 10
+end
 
 
-# puts "Coupons on hand"
-# coupons.each do |coupon|
-# 	puts "Get #{coupon[:item].capitalize} for #{coupon[:cost]} when you by #{coupon[:num]}"
-# end
+puts "Coupons on hand"
+coupons.each do |coupon|
+	puts "Get #{coupon[:item].capitalize} for #{coupon[:cost]} when you by #{coupon[:num]}"
+end
 
-# puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
+puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
