85a48711442e11d0aea8ac41f506456e5600ae45
Sung Bae <singsang2@gmail.com>
2018-10-17 18:13:11 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,62 @@
+
 def consolidate_cart(cart)
-  # code here
+  consolidated = {}
+  cart.each do |item|
+    if !consolidated.keys.include?(item.keys.first)
+      consolidated[item.keys.first] = item.values.first
+    end
+    if consolidated[item.keys.first].keys.include?(:count)
+      consolidated[item.keys.first][:count] += 1
+    else
+      consolidated[item.keys.first][:count] = 1
+    end
+  end
+  consolidated
 end
+puts consolidate_cart([{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+      {"BEETS" => {:price => 2.50, :clearance => false}},
+      {"SOY MILK" => {:price => 4.50, :clearance => true}}])
 
 def apply_coupons(cart, coupons)
-  # code here
+  counter = 0
+  while counter < coupons.length
+    if cart.keys.include?(coupons[counter][:item])
+      if cart[coupons[counter][:item]][:count] >= coupons[counter][:num]
+        cart["#{coupons[counter][:item]} W/COUPON"] = {price: coupons[counter][:cost], clearance:cart[coupons[counter][:item]][:clearance], count: cart[coupons[counter][:item]][:count]/coupons[counter][:num]}
+        if cart[coupons[counter][:item]][:count]%coupons[counter][:num]==0
+          cart.delete(coupons[counter][:item])
+        else
+          cart[coupons[counter][:item][:count]] = cart[coupons[counter][:item]][:count]%coupons[counter][:num]
+        end
+      end
+    end
+    counter +=1 
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, hash|
+    if hash[:clearance]
+      hash[:price] *= 0.8
+    end
+  end
+  cart
 end
-
+  
+  
 def checkout(cart, coupons)
-  # code here
+  
+  new_cart = apply_coupons(consolidate_cart(cart), coupons)
+  new_cart = apply_clearance(new_cart)
+  
+  total = 0
+  
+  new_cart.each{|item, h| total += h[:price]*h[:count]}
+  
+  if total > 100
+    total *= 0.9
+  else
+    total
+  end
 end
