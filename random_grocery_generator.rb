786153af8d793ff92901f26e4593599f3ebb557d
Sung Bae <singsang2@gmail.com>
2018-10-17 17:52:02 +0000

    automatically backed up by learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -84,7 +84,7 @@ end
 def checkout(cart:{}, coupons:{})
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart, coupons)
-  cart = apply_clearnace
+  cart = apply_clearnace(cart)
   
   total = 0
   
